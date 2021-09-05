fe3897f5fbfbe79593378ede4549e429d99f331b
Hasarel Madola <66064707+HasarelMadola@users.noreply.github.com>
2021-09-05 15:08:52 +0530

    resotre the databse

--- modules/client.js
+++ modules/client.js
@@ -2,11 +2,11 @@
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    password: "sql@123",
-    host: "localhost",
+    user: "zizfwtbbzbhyhj",
+    password: "0e698de835a83b7b2e1f846222c93e51f6e5ab4ea0cfc34ebfd9fd3ec2d48699",
+    host: "ec2-50-17-255-244.compute-1.amazonaws.com",
     post: 5432,
-    database: "farmers"
+    database: "dhpu3q9v4hnj6"
 });
 
 module.exports = pool;
