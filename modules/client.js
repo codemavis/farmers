b89904c015e0acc6833434507497cd6b0896d307
Hasarel Madola <66064707+HasarelMadola@users.noreply.github.com>
2021-09-05 15:14:35 +0530

    update ssl in client.js

--- modules/client.js
+++ modules/client.js
@@ -6,7 +6,8 @@ const pool = new Pool({
     password: "0e698de835a83b7b2e1f846222c93e51f6e5ab4ea0cfc34ebfd9fd3ec2d48699",
     host: "ec2-50-17-255-244.compute-1.amazonaws.com",
     post: 5432,
-    database: "dhpu3q9v4hnj6"
+    database: "dhpu3q9v4hnj6",
+    ssl: true
 });
 
 module.exports = pool;
